ac992cb4fcd7af629b42818e72f29f2671cd2738
ahmed sayed <ahmedsayed124680@gmail.com>
2024-05-14 06:56:52 +0300

    make image dynamically can change name at proces

--- all_files/img/views.py
+++ all_files/img/views.py
@@ -4,6 +4,27 @@ from .models import *
 from django.db.models import Q
 from .forms import *
 from django.urls import reverse_lazy
+from django.views.decorators.csrf import csrf_exempt
+import json
+from django.http import Http404, HttpResponseRedirect, JsonResponse
+
+
+
+@csrf_exempt
+def byUserimgUpdateName(request):
+    if request.method == "POST":
+        data                    = json.loads(request.body.decode('utf-8'))
+        old_img_name                   = data.get('activatedImgId')
+        new_image_name                = data.get('newImgName')
+        Img.objects.filter(name=old_img_name).update(name=new_image_name)
+
+        response_data = {
+            'status': 'success',
+        }
+
+    return JsonResponse(response_data)
+
+
 
 class imgDeleteView(DeleteView):
     model           =   Img
@@ -33,6 +54,8 @@ class imgListView(ListView):
     model               =   Img
     template_name       =   'img/img_list.html'
     context_object_name =   'imgs'
+
+    
 
     def get_queryset(self):
         queryset        =   super().get_queryset()
